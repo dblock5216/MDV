0c77e1ac02a5820be163d2a2b153d92e841ef0c7
Dorian Lane <dorianlane@Dorians-MacBook-Pro.local>
2012-04-07 19:53:51 -0500

    Added first files to the repo.

---
+++ lane_dorian_assignment1.js
@@ -0,0 +1,130 @@
+//Project 2 Scalable Data Infrastructures//
+//Dorian Lane April 7, 2012//
+//SDI- Week 2
+
+var  jumpMan
+	 pipPen
+	 graNt
+	 games
+	 points
+	 mjTeam
+	 record  
+	 rinGs
+	 jazz
+	 karlMalone
+     winnEr
+	 foul
+	 foulout
+	 done
+	 didit
+	 
+	
+	jumpMan = M.J,
+	pipPen = 2,
+	graNt = 2,
+	games = 1,
+	points,
+	mjTeam = "daBulls",
+	record = 64,
+	rinGs = 6,
+	jazz = "scrubs",
+	karlMalone = 0,
+	winnEr = true,
+	foul = function (totalFouls) {
+	foulOut = 6,
+	done = "You have fouled out of the game",
+	didit = jumpMan + "just sealed his fate in the Hall of Fame"
+	
+	personalFoul;
+	
+	console.log("Welcome everyone to the 2014 NBA Finals!!!!");
+	console.log("Tonight we feature" + jumpMan + "and" + mjTeam + "vs the " + jazz);
+	
+	//The Setup
+	console.log("Introducing from North Carolina......", jumpMan)
+	console.log("He plays SG for the...", mjTeam)
+	console.log("On his hands are the championships he has won, he has....", rinGs )
+	console.log("Too bad for everyone else, I think they have....", karlMalone)
+	console.log("And he did not come alone, he brought ". pipPen, "and", graNt, "with hiim")
+	console.log("Was MJ a winner?", winnEr)
+
+
+if 	
+	(pipPen = graNt) {console.log("His airness is about to take the court with" + mjTeam);
+else {console.log("We need more bodies")
+
+};
+
+if (rinGs > karlMalone) {console.log("Anyone need to borrow a ring?")};
+
+	else {console.log("Hard work and practice is will bring me to the top")
+};
+
+var shot = [Hook, foulShot, jumpShot, Three], 	
+	feet = [6, 12, 18, 24],
+	makeShot = function (feet[], shot[]) {
+	console.log(jumpMan + "jumst made a " + shot + "from" + feet + "feet!");
+}
+	
+	makeshot (foulShot, 12),
+	makeshot (Three, 24),
+	makeShot (Hook, 18),
+	makeShot (jumpShot, 6);
+
+var totalPoints = makeShot();	
+
+var getPpg = function (p) {
+	var ppg = points * game;
+	return ppg;
+	
+if (totalPoints > record) {
+	return didit;
+}
+else {console.log("What a shame to come so close");
+}
+
+getPpq();
+
+if (p > 22) {console.log("Wow what an all star player, no wonder he has" + rinGs + "championships");
+else {console.log("Really guy you only scored" + p + "a quarter!");
+}
+
+var quarters = [" 1st Quarter", "2nd Quarter", "3rd Quarter", "4th Quarter"];
+ppq = [11, 5, 12, 8];
+
+for (var quarternumber = 0;) quarternumber < quarters.length; quarternumber++) {
+	return ppq;    
+
+if (totalFouls > foulOut) {
+	var foul = function (totalFouls) {
+	var foulOut = 6,
+	var done = "You have fouled out of the game",
+	personalFoul
+	console.log(done)
+	foul = 0;
+
+	else {personalFoul = math.floor(foulOut - totalFouls);
+	return personalFoul;
+};
+
+var gotFouls = foul();
+	console.log("You have got" + gotFouls + "personal fouls");
+	
+	gotFouls(7);
+
+
+//Boolean
+	if (winnEr === true) {
+		console.log(mjTeam + "have won the championship!!!!")
+	};
+
+	else (winner === false) {
+		console.log(mjTeam + "has lot the NBA Finals, who would have ever thought!!")
+	}
+	return winner;
+	
+
+
+
+
+
