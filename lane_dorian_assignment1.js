7981183c7b47e73c8d1bdfe4b81e2c8a3c8cd7ac
Dorian Lane <dorianlane@Dorians-MacBook-Pro.local>
2012-04-23 13:10:27 -0500

    added flowchart

--- lane_dorian_assignment1.js
+++ lane_dorian_assignment1.js
@@ -1,6 +1,6 @@
 //Project 2 Scalable Data Infrastructures//
-//Dorian Lane April 7, 2012//
-//SDI- Week 2
+//Dorian Lane April 18, 2012//
+//SDI- Week 3
 
 var  jumpMan
 	 pipPen
@@ -17,8 +17,10 @@ var  jumpMan
 	 foulout
 	 done
 	 didit
-	 
-	
+// end section 1
+
+
+// beginning section 2	
 	jumpMan = M.J,
 	pipPen = 2,
 	graNt = 2,
@@ -30,25 +32,26 @@ var  jumpMan
 	jazz = "scrubs",
 	karlMalone = 0,
 	winnEr = true,
-	foul = function (totalFouls) {
 	foulOut = 6,
 	done = "You have fouled out of the game",
-	didit = jumpMan + "just sealed his fate in the Hall of Fame"
+	didit = jumpMan + "just sealed his fate in the Hall of Fame",;
 	
-	personalFoul;
 	
 	console.log("Welcome everyone to the 2014 NBA Finals!!!!");
-	console.log("Tonight we feature" + jumpMan + "and" + mjTeam + "vs the " + jazz);
+	console.log("Tonight we feature" + jumpMan + "and" + mjTeam + "vs the" + jazz);
 	
-	//The Setup
-	console.log("Introducing from North Carolina......", jumpMan)
-	console.log("He plays SG for the...", mjTeam)
-	console.log("On his hands are the championships he has won, he has....", rinGs )
-	console.log("Too bad for everyone else, I think they have....", karlMalone)
-	console.log("And he did not come alone, he brought ". pipPen, "and", graNt, "with hiim")
+// The Setup
+	console.log("Introducing from North Carolina......" + jumpMan)
+	console.log("He plays SG for the..." + mjTeam)
+	console.log("On his hands are the championships he has won, he has...." + rinGs )
+	console.log("Too bad for everyone else, I think they have...."+ karlMalone)
+	console.log("And he did not come alone, he brought " + pipPen + "and" + graNt + "with him")
 	console.log("Was MJ a winner?", winnEr)
 
+// End section 2
 
+
+// beginning section 3
 if 	
 	(pipPen = graNt) {console.log("His airness is about to take the court with" + mjTeam);
 else {console.log("We need more bodies")
@@ -60,40 +63,68 @@ if (rinGs > karlMalone) {console.log("Anyone need to borrow a ring?")};
 	else {console.log("Hard work and practice is will bring me to the top")
 };
 
-var shot = [Hook, foulShot, jumpShot, Three], 	
-	feet = [6, 12, 18, 24],
-	makeShot = function (feet[], shot[]) {
-	console.log(jumpMan + "jumst made a " + shot + "from" + feet + "feet!");
-}
-	
-	makeshot (foulShot, 12),
-	makeshot (Three, 24),
-	makeShot (Hook, 18),
-	makeShot (jumpShot, 6);
+// end section 3
 
-var totalPoints = makeShot();	
+//begin section 4
+var	shot = {
+	"selection": [
+	"three": {
+		"distance": "24 ft",
+		"points": "3 pts",
+		"difficulty": "hard"
+	},
+	"two": {
+		"distance": "16 ft",
+		"points": "2 pts",
+		"difficulty": "medium"
+	},
+	"freethrow": {
+		"distance": "12 ft",
+		"points": "1",
+		"difficulty": "easy"
+				]
+};
 
-var getPpg = function (p) {
-	var ppg = points * game;
-	return ppg;
-	
+var makeshot = function (shot) {
+		for (var i = 0; i < shot.selection.length; i++){
+		var score = shot.selection[i];
+		console.log("distance: " + selection.distance + ", points: " + selection.points + ", difficulty: " + selection.difficulty);
+				}
+};
+
+makeshot(shot);
+
+var shotstring = shot.stringify(shot);
+console.log(shotstring);
+
+var receivedshot = shot.parse(shotstring);
+console.log(receivedshot);
+
+// end section 4
+
+//begin section 5
 if (totalPoints > record) {
 	return didit;
 }
 else {console.log("What a shame to come so close");
 }
 
-getPpq();
+getPpg();
 
 if (p > 22) {console.log("Wow what an all star player, no wonder he has" + rinGs + "championships");
+}
 else {console.log("Really guy you only scored" + p + "a quarter!");
 }
 
-var quarters = [" 1st Quarter", "2nd Quarter", "3rd Quarter", "4th Quarter"];
-ppq = [11, 5, 12, 8];
+// end section 5
 
-for (var quarternumber = 0;) quarternumber < quarters.length; quarternumber++) {
-	return ppq;    
+
+// begin section 6
+
+for (var quarternumber = 0) (quarternumber < quarters.length; quarternumber++) {
+	}
+	return ppq[];  
+}
 
 if (totalFouls > foulOut) {
 	var foul = function (totalFouls) {
@@ -102,27 +133,28 @@ if (totalFouls > foulOut) {
 	personalFoul
 	console.log(done)
 	foul = 0;
-
+}
 	else {personalFoul = math.floor(foulOut - totalFouls);
+}	
 	return personalFoul;
 };
 
 var gotFouls = foul();
-	console.log("You have got" + gotFouls + "personal fouls");
+	console.log( "You have got" + gotFouls + "personal fouls" );
 	
 	gotFouls(7);
 
 
-//Boolean
-	if (winnEr === true) {
-		console.log(mjTeam + "have won the championship!!!!")
+//Start section 7
+	if (winner === true) {
+		console.log( mjTeam + "have won the championship!!!!" )
 	};
 
 	else (winner === false) {
-		console.log(mjTeam + "has lot the NBA Finals, who would have ever thought!!")
+		console.log( mjTeam + "has lot the NBA Finals, who would have ever thought!!" )
 	}
 	return winner;
-	
+// end section 7
 
 
 
