346258253ac8f10b9c5e43cafbb4a4532e7d4f1b
dblock5216@gmail.com <acap10242011>
2012-04-27 01:16:58 -0500

    added volume 1 to lib

---
+++ lane_dorian_assignment1.js
@@ -0,0 +1,160 @@
+//Project 3 Scalable Data Infrastructures//
+//Dorian Lane April 18, 2012//
+//SDI- Week 3
+
+var      jumpMan
+	 pipPen
+	 graNt
+	 games
+	 points
+	 mjTeam
+	 record  
+	 rinGs
+	 jazz
+	 karlMalone
+         winnEr
+	 foul
+	 foulout
+	 done
+	 didit
+// end section 1
+	 
+// beginning section 2	
+	jumpMan = M.J,
+	pipPen = 2,
+	graNt = 2,
+	games = 1,
+	points,
+	mjTeam = "daBulls",
+	record = 64,
+	rinGs = 6,
+	jazz = "scrubs",
+	karlMalone = 0,
+	winnEr = true,
+	foulOut = 6,
+	done = "You have fouled out of the game",
+	didit = jumpMan + "just sealed his fate in the Hall of Fame",;
+	
+	
+	console.log("Welcome everyone to the 2014 NBA Finals!!!!");
+	console.log("Tonight we feature" + jumpMan + "and" + mjTeam + "vs the" + jazz);
+	
+// The Setup
+	console.log("Introducing from North Carolina......" + jumpMan)
+	console.log("He plays SG for the..." + mjTeam)
+	console.log("On his hands are the championships he has won, he has...." + rinGs )
+	console.log("Too bad for everyone else, I think they have...."+ karlMalone)
+	console.log("And he did not come alone, he brought " + pipPen + "and" + graNt + "with him")
+	console.log("Was MJ a winner?", winnEr)
+
+// End section 2
+
+// beginning section 3
+if 	
+	(pipPen = graNt) {console.log("His airness is about to take the court with" + mjTeam);
+else {console.log("We need more bodies")
+
+};
+
+if (rinGs > karlMalone) {console.log("Anyone need to borrow a ring?")};
+
+	else {console.log("Hard work and practice is what will bring me to the top")
+};
+
+// end section 3
+
+//begin section 4
+var	shot = {
+	"selection": [
+	"three": {
+		"distance": "24 ft",
+		"points": "3 pts",
+		"difficulty": "hard"
+	},
+	"two": {
+		"distance": "16 ft",
+		"points": "2 pts",
+		"difficulty": "medium"
+	},
+	"freethrow": {
+		"distance": "12 ft",
+		"points": "1",
+		"difficulty": "easy"
+				]
+};
+
+var makeshot = function (shot) {
+		for (var i = 0; i < shot.selection.length; i++){
+		var score = shot.selection[i];
+		console.log("distance: " + selection.distance + ", points: " + selection.points + ", difficulty: " + selection.difficulty);
+				}
+};
+
+makeshot(shot);
+
+var shotstring = shot.stringify(shot);
+console.log(shotstring);
+
+var receivedshot = shot.parse(shotstring);
+console.log(receivedshot);
+
+// end section 4
+	
+//begin section 5
+if (totalPoints > record) {
+	return didit;
+}
+else {console.log("What a shame to come so close");
+}
+
+getPpg();
+
+if (p > 22) {console.log("Wow what an all star player, no wonder he has" + rinGs + "championships");
+}
+else {console.log("Really guy you only scored" + p + "a quarter!");
+}
+
+// end section 5
+
+// begin section 6
+
+var quarters = [" 1st Quarter", "2nd Quarter", "3rd Quarter", "4th Quarter"];
+ppq = [11, 5, 12, 8];
+
+for (var quarternumber = 0;) quarternumber < quarters.length; quarternumber++) {
+	return ppq[0];    
+
+if (totalFouls > foulOut) {
+	var foul = function (totalFouls) {
+	var foulOut = 6,
+	var done = "You have fouled out of the game",
+	personalFoul
+	console.log(done)
+	foul = 0;
+
+	else {personalFoul = math.floor(foulOut - totalFouls);
+	return personalFoul;
+};
+
+var gotFouls = foul();
+	console.log("You have got" + gotFouls + "personal fouls");
+	
+	gotFouls(7);
+	
+// end section 6
+
+//Start section 7
+	if (winner === true) {
+		console.log( mjTeam + "have won the championship!!!!" )
+	};
+
+	else (winner === false) {
+		console.log( mjTeam + "has lot the NBA Finals, who would have ever thought!!" )
+	}
+	return winner;
+// end section 7
+
+
+
+
+
