2215174652428706374c52cee6122e195e92f275
dblock5216@gmail.com <acap10242011>
2012-05-17 21:15:02 -0500

    added new files

---
+++ Lane_Dorian_Assignment3.js
@@ -0,0 +1,22 @@
+//Dorian Lane
+//May 17th, 2012
+//SDI-Week 3 Project 3
+
+
+
+var noob = "PVT Jorge Manada",
+	tired = true,
+	days = 3,
+	home = "Reception Company",
+	georgia = "Capo",
+	where = "Fort Lewis",
+	car  = "Chevy Trailblazer",
+	team = noob + georgia;
+	
+ 
+	
+	console.log(georgia +  " thought it would be a good idea to drive to " + where + "."),                                                                                                                                                                                                                                                                  
+	console.log("Lets just in the " + car + " says " + georgia + " or you will be here for the next nine weeks "),
+	console.log("Haha, funny guy"),
+	console.log(team + "  ..made the	 " + days + " day trip to " + where + ", what road warriors! ");
+
