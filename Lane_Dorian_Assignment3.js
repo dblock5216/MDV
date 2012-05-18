b3dff412f211dc31173598667f86014836b57f54
dblock5216@gmail.com <acap10242011>
2012-05-17 21:16:16 -0500

    added new function and strings

--- Lane_Dorian_Assignment3.js
+++ Lane_Dorian_Assignment3.js
@@ -20,3 +20,47 @@ var noob = "PVT Jorge Manada",
 	console.log("Haha, funny guy"),
 	console.log(team + "  ..made the	 " + days + " day trip to " + where + ", what road warriors! ");
 
+
+if (tired = true) {
+	console.log(" Man, what a long journey!! ")
+}
+else {console.log(" Looks like the Army has changed from when my father was in")
+}
+var daysallowed = 4
+if (days > daysallowed) {
+	console.log( " Wonderful, we both have made it with enough time remaining, so we both get bonuses.") 
+} 
+else {console.log("What took you us so long?");
+};
+
+var speed = 1;
+var lap = 1;
+
+console.log("The next morning at pt...");
+var runlaps = function (m) {
+	var maxedout = m * speed
+	console.log(" Wow " + noob +  " you ran " + maxedout + " laps.")
+	return;runlaps
+};
+
+runlaps(7);
+
+
+console.log("It's hard being the new guys in the unit... ")
+var  hotseat = function () {
+	console.log(noob + " and" + georgia + " are in the spotlight");
+    return;hotseat
+};
+hotseat();
+
+console.log("We need 2 soldiers to do 8 hour guard duty on Sergeant Major..." + noob + " I'm sending you."); 
+
+
+var didyouguard = function(t) {
+	if (t > 8) {
+		console.log("Good job you have completed the watch.")
+};
+ 
+return;didyouguard;
+didyouguard(9);
+};
