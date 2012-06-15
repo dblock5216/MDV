90e7fdbb391a5efa7cc420effeb76ca6b443c572
dblock5216@gmail.com <acap10242011>
2012-06-14 21:14:52 -0500

    added week 3 files

--- Lane_Dorian_Assignment3.js
+++ Lane_Dorian_Assignment3.js
@@ -1,5 +1,5 @@
 //Dorian Lane
-//May 17th, 2012
+//June 14th, 2012
 //SDI-Week 3 Project 3
 
 
@@ -11,114 +11,13 @@ var noob = "PVT Jorge Manada",
 	georgia = "Capo",
 	where = "Fort Lewis",
 	car  = "Chevy Trailblazer",
-	team = noob + georgia;
+	team = noob + georgia,
+	var speed = 1,
+	var lap = 1,
+	trouble = true,
+	var miles = 15,
+	var endpoint = 0,
 	
- 
-	
-	console.log(georgia +  " thought it would be a good idea to drive to " + where + "."),                                                                                                                                                                                                                                                                  
-	console.log("Lets just in the " + car + " says " + georgia + " or you will be here for the next nine weeks "),
-	console.log("Haha, funny guy"),
-	console.log(team + "  ..made the	 " + days + " day trip to " + where + ", what road warriors! ");
-
-
-if (tired = true) {
-	console.log(" Man, what a long journey!! ")
-}
-else {console.log(" Looks like the Army has changed from when my father was in")
-}
-var daysallowed = 4
-if (days > daysallowed) {
-	console.log( " Wonderful, we both have made it with enough time remaining, so we both get bonuses.") 
-} 
-else {console.log("What took you us so long?");
-};
-
-var speed = 1;
-var lap = 1;
-
-console.log("The next morning at pt...");
-var runlaps = function (m) {
-	var maxedout = m * speed
-	console.log(" Wow " + noob +  " you ran " + maxedout + " laps.")
-	return;runlaps
-};
-
-runlaps(7);
-
-
-console.log("It's hard being the new guys in the unit... ")
-var  hotseat = function () {
-	console.log(noob + " and" + georgia + " are in the spotlight");
-    return;hotseat
-};
-hotseat();
-
-console.log("We need 2 soldiers to do 8 hour guard duty on Sergeant Major..." + noob + " I'm sending you."); 
-
-
-var didyouguard = function(t) {
-	if (t > 8) {
-		console.log("Good job you have completed the watch.")
-};
- 
-return;didyouguard;
-didyouguard(9);
-};
-
-
-console.log("I have to do better than this, this is what I have always wanted.");
-console.log("The next week....");
-
-var beatduty = function (true) {
-    console.log(noob + " you did it, I feel as if i can trust you now.");
-};
-beatduty("true");
-
-//combatives tournament
-	console.log("Word of a combatives tournament arose and " + noob + " always had a passion for fighting");
-	console.log("I'm gonna join, " + noob + " said. But I must be ready");
-var  getready = function () {
-	console.log("TRAINING FOR NEXT 2 WEEKS")
-        return;getready
-};
-
-getready();
-console.log("Finally ready, " + noob + "showed up the day of the tournament");
-console.log(georgia + could barely watch as he heard the results of each match");
-	
-	
-var welterweight = ["Mike", "Charles", "Jim", "Chad"],
-	fighttimes = [ 60, 30, 120, 90];
-var entertourney = function () {
-	var howin = welterweight[fight],
-		results = fighttimes[fight];
-		console.log( noob + " beat " + howin + " in " + results + " seconds!");
-}
-var welterweight = ["Mike", "Charles", "Jim", "Chad"],
-	fighttimes = [ 60, 30, 120, 90];
-for (var fight = 0; fight< welterweight.length; fight++) {
-	entertourney();
-}
-console.log("noob + " won!");
-console.log("After winning the tournament, " + noob + " decided to try the post ruck march.");
-var miles = 15
-var endpoint = 0
-var journey = function () {
-while (miles > endpoint) {
-	console.log("Not too much longer only " + miles + " miles left to go!")
-	 miles--;
-};
-
-if (miles === endpoint) {
-	console.log("You have finished the road march.")
-}
-	};
-	journey();
-
-
-console.log("After winning the tournament things were looking up for " + noob + ", he even got some new squadmates");
-console.log("Whats your names and where are you from?" + noob + " said with a big grin");
-
 var battleroster = {
     "battlebuddies": {
 	"0000001":
@@ -140,19 +39,5 @@ var battleroster = {
 	"age": 19
         }
     }};
-
-for (var key in battleroster.battlebuddies) {
-	var battlebuddy = battleroster.battlebuddies[key]
-};
-
-
-var roster = [ battleroster.battlebuddies["0000001"],
-battleroster.battlebuddies["0000002"],
-battleroster.battlebuddies["0000003"]
-];
-var assign = function(list) {
-console.log(list);
-};
-assign(roster);
-
-console.log("I think were gonna have some fun with you guys, welcome to " + where + "!"
+	
+ 
